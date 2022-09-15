33fdcd54cf07adc29b4b360f22511e6d862e06e9
Fobek <quicksilverx@yandex.ru>
2022-09-15 16:08:29 +0300

    Вернул infinite

--- js/script.js
+++ js/script.js
@@ -1,11 +1,10 @@
 $(document).ready(function() {
     $('.works').slick({
         adaptiveHeight: true,
-        infinite: false,
         slidesToShow: 6,
         responsive:[
             {breakpoint: 1300,
-                settings: {slidesToShow: 4}}
+            settings: {slidesToShow: 4}}
         ]
-    }); 
+    });
 })
