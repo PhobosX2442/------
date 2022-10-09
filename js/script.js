3097e4142a3d6e13b4d2e979cf14db5b17eb2bb0
Fobek <quicksilverx@yandex.ru>
2022-10-09 20:07:59 +0300

    +Читать далее, +Скрытие

--- js/script.js
+++ js/script.js
@@ -72,3 +72,20 @@ if (scroll >= 1) {
         }
     });
 });
+
+// Кнопка "Читать далее"
+function myFunction() {
+    var dots = document.getElementById("dots");
+    var moreText = document.getElementById("more");
+    var btnText = document.getElementById("myBtn");
+
+    if (dots.style.display === "none") {
+    dots.style.display = "inline";
+    btnText.innerHTML = "Читать больше";
+    moreText.style.display = "none";
+    } else {
+    dots.style.display = "none";
+    btnText.innerHTML = "Читать меньше";
+    moreText.style.display = "inline";
+    }
+}
